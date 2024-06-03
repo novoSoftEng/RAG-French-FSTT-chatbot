722572d5e3381b8746b61543d048b8716f6b6be2
secr <ch.liefrid@gmail.com>
2024-06-03 18:55:59 +0100

    dockerization

--- api/RAG.py
+++ api/RAG.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-
+import re
 from IPython.display import display, Markdown
 import pandas as pd
 
@@ -15,7 +15,11 @@ import pandas as pd
 
 
 import os
+def extract_answer_from_text(text):
+    answer_match = re.search(r'Answer:\s*(.*)', text, re.DOTALL)
+    answer = answer_match.group(1).strip() if answer_match else ""
 
+    return answer
 # Set your Hugging Face API token
 HUGGINGFACE_HUB_TOKEN = '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'
 os.environ['HUGGINGFACE_HUB_TOKEN'] = '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'
@@ -26,32 +30,35 @@ os.environ['HUGGINGFACE_HUB_TOKEN'] = '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'
 
 # model_name="google/gemma-2b-it"
 # model_name="aymanboufarhi/gemma-fstt"
+
 class AIAgent:
-    """
-    Gemma 2b-it assistant.
-    It uses Gemma transformers 2b-it/2.
-    """
-    def __init__(self, model_name="aymanboufarhi/gemma2B-chat-bot-fstt", max_length=256):
+    
+    def __init__(self, model_name="aymanboufarhi/gemma2B-chat-bot-fstt", max_length=1000):
         self.max_length = max_length
         try:
-            self.tokenizer = AutoTokenizer.from_pretrained(model_name,token = HUGGINGFACE_HUB_TOKEN)
-            self.gemma_llm = AutoModelForCausalLM.from_pretrained(model_name,token = HUGGINGFACE_HUB_TOKEN)
+            self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=HUGGINGFACE_HUB_TOKEN)
+            self.gemma_llm = AutoModelForCausalLM.from_pretrained(model_name, token=HUGGINGFACE_HUB_TOKEN)
         except Exception as e:
             raise ValueError(f"Error loading model: {e}")
 
-    # Do not include other information.
     def create_prompt(self, query, context):
         # Prompt template
         prompt = f"""
-        Vous étes un AI qui connait tout sur ce qui concerne la FSTT , FST de Tanger , les actualités de la fstt , les formation ,
-        les prof / professeur , tu va repondre dans le context si possible à la Question possible.\n 
+        You are an AI Agent specialized to answer questions about FSTT (faculty of science and technology in Tanger).
+        Explain the concept or answer the question about FSTT.
+        In order to create the answer,use the information from the context if it seems to be relevant to the question provided (Context). 
+        and the context will be as a list, so you must use just the most relevent informations from the list.
+        Answer with simple words.
+        If needed, include also explanations.
+        it's important to answer with french languge.
+        return only the answer
         Question: {query}
         Context: {context}
         Answer:
         """
         return prompt
     
-    def generate(self, query, retrieved_info):
+    def generate(self, query, retrieved_info=None):
         prompt = self.create_prompt(query, retrieved_info)
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
         
@@ -60,8 +67,8 @@ class AIAgent:
         
         # Decode and return the answer
         answer = self.tokenizer.decode(answer_ids[0], skip_special_tokens=True)
-        return prompt, answer
-
+        
+        return answer
 
 # ### Test the AIAgent :
 
@@ -83,52 +90,27 @@ class RAGSystem:
     """Sentence embedding based Retrieval Based Augmented generation.
        Given a ChromaDB collection, retriever finds num_retrieved_docs relevant documents."""
     
-    def __init__(self, ai_agent, collection = connect(), num_retrieved_docs=2):
+    def __init__(self, ai_agent, collection=connect(), num_retrieved_docs=2):
         self.num_docs = num_retrieved_docs
         self.collection = collection
         self.ai_agent = ai_agent
-        self.template = "\n\nQuestion:\n{question}\n\nPrompt:\n{prompt}\n\nAnswer:\n{answer}\n\nContext:\n{context}"
+    
     def retrieve(self, query):
         # Retrieve top k similar documents to query
         results = self.collection.query(query_texts=[query], n_results=self.num_docs)
         docs = [result for result in results['documents']]
         return docs
-        
-
     
     def query(self, query):
         # Generate the answer
-        context_docs  = self.retrieve(query)
+        context_docs = self.retrieve(query)
         context_docs = context_docs[0]
-        # unique_docs = self.deduplicate_docs(context)
-        # # unique_docs = set(context_docs)
-        # print(unique_docs)
-        data = ""
-        for item in list(context_docs):
-            data += item
-        context = " | ".join(context_docs[:self.num_docs])
+        print(context_docs)
         
-        # prompt = f"""
-        # You are an AI Agent specialized to answer questions about FSTT (faculty of science and technology in Tanger).
-        # Explain the concept or answer the question about FSTT.
-        # In order to create the answer, please only use the information from the
-        # context provided (Context). Do not include other information.
-        # Answer with simple words.
-        # It's important to answer with french languge.
-        # If needed, include also explanations.
-        # Question: {query}
-        # Context: {context}
-        # Answer:
-        # """
+        answer = self.ai_agent.generate(query, context_docs)
+
         
-        # input_ids = self.collection.embedding_function.tokenizer(prompt, return_tensors="pt").input_ids
-        # answer_ids = self.collection.embedding_function.model.generate(input_ids, max_new_tokens=256)
-        # answer = self.collection.embedding_function.tokenizer.decode(answer_ids[0], skip_special_tokens=True)
-
-        prompt, answer = self.ai_agent.generate(query, context)
-        print("answer : ", answer)
-        return self.template.format(question=query, prompt=prompt, answer=answer, context=context)
-
+        return answer
 
 # In[8]:
 
--- api/api.py
+++ api/api.py
@@ -1,26 +1,58 @@
 import sys
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import gc
 # Append the path for the RAG module
-from RAG import AIAgent, RAGSystem 
-# Initialize the AI Agent
-ai_agent = AIAgent()
-# Initialize the RAGSystem with the existing collection
-rag_system = RAGSystem(ai_agent=ai_agent, num_retrieved_docs=4)
-# Initialize FastAPI app
+sys.path.append('../RAG')
+from RAG import AIAgent, RAGSystem, extract_answer_from_text 
+
+# Initialize global variables
+ai_agent = None
+rag_system = None
+
 # Initialize the Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+@app.route("/model", methods=["POST"])
+def initialize_model():
+    global ai_agent, rag_system
+    try:
+        # Get the model name from the request JSON
+        data = request.json
+        model = data.get("model", "")
+        del ai_agent
+        del rag_system
+        gc.collect()
+        if model == "rag":
+            try:
+                # Initialize the AI Agent
+                ai_agent = AIAgent(model_name="google/gemma-2b-it")
+                # Initialize the RAGSystem with the existing collection
+                rag_system = RAGSystem(ai_agent=ai_agent, num_retrieved_docs=4)
+                return jsonify({"response": True})
+            except Exception as e:
+                return jsonify({"response": False, "error": str(e)})
+        else:
+            ai_agent = AIAgent()
+            rag_system = None
+            return jsonify({"response": True})
+    except Exception as e:
+        # Return an error response
+        return jsonify({"error": str(e)}), 500
+
 @app.route("/query", methods=["POST"])
-def generate_answer():
+def handle_query():
+    global ai_agent, rag_system
     try:
         # Get the prompt from the request JSON
         data = request.json
         prompt = data.get("prompt", "")
-
-        # Query the RAG system with the provided prompt
-        res = rag_system.query(prompt)
+        if rag_system is None:
+            res = ai_agent.generate(prompt)
+        else:
+            res = rag_system.query(prompt)
+        res = extract_answer_from_text(res)
 
         # Return the response
         return jsonify({"response": res})
@@ -31,3 +63,4 @@ def generate_answer():
 # Run the app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8500)
+
